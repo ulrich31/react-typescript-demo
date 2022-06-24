3e49d30036ddc7ccea5ca864e0a16e2787bfb95a
Ulrich Manantsoa <ulrich.thirty@gmail.com>
2022-06-24 10:13:24 +0300

    10

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import { Button } from './components/Button';
+import { Container } from './components/Container';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
+import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { User } from './components/state/User';
 import { Status } from './components/Status';
 
 function App() {
@@ -38,6 +42,12 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to Leo</Heading>
       </Oscar>
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id)
+      }}></Button>
+      <Input value='' handleChange={event => console.log(event)} />
+      <Container styles={{ border: '1px solid black', padding: '1rem' }}/>
+      <User />
     </div>
   )
 }
